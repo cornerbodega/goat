118b23be1beadb155fad6213b2cf19754ff2f351
cornerbodega <merhone@gmail.com>
2015-07-23 17:33:16 -0500

    added weight rounding to 2 decimal places

---
+++ php/inventory/scheduleDestroy.php
@@ -0,0 +1,33 @@
+<?php
+$params = json_decode(file_get_contents('php://input'),true);
+
+
+$curl = curl_init();
+$id = $params['id'];
+$reason = $params['reason'];
+$action = $params['action'];
+$session = $params['session'];
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://wslcb.mjtraceability.com/serverjson.asp",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => "{\n\"API\": \"4.0\",\n\"action\": \"".$action."\",\n\"barcodeid\": ". $id .",\n\"reason\": \"". $reason ."\",\n\"sessionid\":\"".$session."\"\n}",
+  CURLOPT_HTTPHEADER => array(
+    "content-type: application/json"
+  ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
+
